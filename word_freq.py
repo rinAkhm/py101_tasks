9e08f232a758b682c8242df73721f92b08c606bf
Ринат Ахметзянов <r.akhmetzyanov@innopolis.ru>
2020-11-06 15:14:55 +0300

    style: add documentation for word_freq.py

--- word_freq.py
+++ word_freq.py
@@ -13,7 +13,7 @@ import collections
 """
 
 def exclude_stop_words(text):
-# функция исключает стоп слова из ипортированного списка nltk 
+    """Функция исключает стоп слова из ипортированного списка nltk."""
     stop_words = stopwords.words ("russian")
     list_without_stop_words = []
     for word in text:
@@ -22,16 +22,16 @@ def exclude_stop_words(text):
     return list_without_stop_words
 
 def create_parser():
-#Функция считывает название файла через консоль и открывает этот файл. 
-#второй параметор позволяет выбрать количество чисел для считывания
+    """Функция считывает название файла через консоль и открывает этот файл. 
+    второй параметор позволяет выбрать количество чисел для считывания"""
     parser = argparse.ArgumentParser()
     parser.add_argument('-b','--book', type=argparse.FileType(encoding='utf-8'), help='you need write name file', default='book.txt')
     parser.add_argument ('count', nargs='?', type = int, default=100)
     return parser
 
 def create_clean_list(text):
-#Функция разбирает текст на слова, избавляет текст от
-#  символов и цифр, после сохраняет его в список. 
+    """Функция разбирает текст на слова, избавляет текст от
+    символов и цифр, после сохраняет его в список."""
     list_without_symbols = []
     for word in text.lower().split():
         symbols = '-!@#№$<>%^"\'*.()_+="№;:,?'
@@ -49,12 +49,12 @@ if __name__ == '__main__':
     text = " ".join(namespace.book.readlines())
     count = namespace.count
 
-#добавляем слова в словарь 
+    """Добавляем слова в словарь."""
     for word_dictionary in exclude_stop_words(create_clean_list(text)): 
         dictionary.setdefault (word_dictionary, 0)
         dictionary[word_dictionary] +=1
 
-#сортируем словарь и выводим в принт
+    """Сортируем словарь и выводим в при помощи принт."""
     top_word = collections.Counter(dictionary).most_common(count)
     print(top_word)
  
